8d6d09082638a2d12357803cc2f317358154379c
AkiraBrown <akira.watson.brown@gmail.com>
2024-08-13 13:45:49 -0400

    refactor(blogController): applied date format to get all blogs request

--- src/controllers/blog.controller.ts
+++ src/controllers/blog.controller.ts
@@ -2,20 +2,20 @@ import express, { Request, Response } from "express";
 const fs = require("node:fs");
 const marked = require("marked");
 const router = express.Router();
-const {
-  getAllBlogs,
-  getOneBlog,
-  updateBlog,
-  createBlog,
-  deleteBlog,
-} = require("../queries/blog");
+const { getAllBlogs, getOneBlog, createBlog } = require("../queries/blog");
 
 router.get("/", async (_: Request, res: Response) => {
   try {
     const result = await getAllBlogs();
-    result.forEach((element) => {
-      delete element.file_path;
-    });
+    result.forEach(
+      (element: { date_uploaded: string | number | Date; file_path: any }) => {
+        delete element.file_path;
+        const year = new Date(element.date_uploaded).getUTCFullYear();
+        const month = new Date(element.date_uploaded).getUTCMonth() + 1;
+        const day = new Date(element.date_uploaded).getUTCDate();
+        element.date_uploaded = `${month}/${day}/${year}`;
+      }
+    );
     res.status(200).json(result);
   } catch (error) {
     res.status(500).json(error);
