80380a2f00dea28da9c8ecf31b2ab2cc96650ea9
AkiraBrown <akira.watson.brown@gmail.com>
2024-08-14 15:34:04 -0400

    fix(dist): changed parts of dist file to support js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-var app = require("./src/app.ts");
+var app = require("./src/app");
 require("dotenv").config();
 var PORT = process.env.PORT || 8080;
 app.listen(PORT, function () {
-    console.log("Server is running on port ".concat(PORT));
+  console.log("Server is running on port ".concat(PORT));
 });
 module.exports = app;
